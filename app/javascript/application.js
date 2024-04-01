796c80553daeb926eb31657d383311ae470ed16e
Gustavo Cunha <dev@gustavocunha.dev>
2024-04-01 20:43:36 +0200

    Add budgeted and activity on category view
    Also, move code from controller to helper. This will improve code testability

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,40 +1,44 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
 
-import Highcharts from "highcharts"
+import Highcharts from "highcharts";
 
-window.Highcharts = Highcharts
+window.Highcharts = Highcharts;
 
-document.addEventListener('turbo:frame-load', function () {
-    Highcharts.setOptions({
-        lang: {
-            decimalPoint: ',',
-            thousandsSep: '.'
+document.addEventListener("turbo:frame-load", function () {
+  Highcharts.setOptions({
+    lang: {
+      decimalPoint: ",",
+      thousandsSep: ".",
+    },
+  });
+  if (document.getElementById("chart")) {
+    const chart = {
+      title: { text: title },
+      subtitle: { text: subtitle },
+      chart: { type: type, height: 700, curve: true },
+      accessibility: { enabled: false },
+      xAxis: {
+        categories: dates,
+        type: "datetime",
+      },
+      yAxis: {
+        labels: {
+          formatter: function () {
+            return new Intl.NumberFormat("en-DE", {
+              style: "currency",
+              currency: "EUR",
+            }).format(this.value);
+          },
         },
-    });
-    if(document.getElementById('chart')){
-        const chart = {
-            title: { text: title },
-            subtitle: { text: subtitle },
-            chart: { type: type, height: 700, curve: true},
-            xAxis: {
-                categories: dates,
-                type: 'datetime'
-            },
-            yAxis: {
-                labels: {
-                    formatter: function() {
-                        return new Intl.NumberFormat('en-DE', { style: 'currency', currency: 'EUR' }).format(this.value)
-                    }
-                },
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.y}</b><br/>',
-                valuePrefix: '€',
-            },
-            series: values
-        }
-        Highcharts.chart('chart', chart)
-    }
-})
+      },
+      tooltip: {
+        pointFormat: "{series.name}: <b>{point.y}</b><br/>",
+        valuePrefix: "€",
+      },
+      series: values,
+    };
+    Highcharts.chart("chart", chart);
+  }
+});
